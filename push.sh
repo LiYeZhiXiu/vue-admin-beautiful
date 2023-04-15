ce7474e28e27ef6183d4a9731e1277f96c5b50e4
LiYeZhiXiu <3201534466@qq.com>
2023-04-15 11:01:50 +0800

    🎉 feat: init project

--- push.sh
+++ push.sh
@@ -7,11 +7,11 @@
 # git push -f "https://${access_token}@github.com/LiYeZhiXiu/vue-admin-beautiful.git" master
 # exec /bin/bash
 # echo "# vue-admin-beautiful" >> README.md
-set -e
+# set -e
 # git init
 git add .
 git commit -m '🎉 feat: init project'
-git push -f "https://${access_token}@github.com/LiYeZhiXiu/vue-admin-beautiful.git" master
+git push -f "https://${access_token}@github.com/LiYeZhiXiu/vue-admin-beautiful.git" 
 
 # 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
 
